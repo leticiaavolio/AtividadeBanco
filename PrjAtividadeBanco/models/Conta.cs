96586b9e654f9274365b9f6458bf6f09cb9e320b
leticia <leticiaavolio09@gmail.com>
2024-10-31 21:20:22 -0300

    Adicionamos herança, além de classe e métodos abstratos

---
+++ PrjAtividadeBanco/models/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetiAvolio.PrjAtividadeBanco.models
+{
+    public abstract class Conta
+    {
+        public string Titular { get; set; }
+        public string NumeroConta { get; set; }
+        public double Saldo { get; set; }
+
+        public void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new InvalidOperationException("#############################\n Saque não permitido. Valor maior que o saldo. \n#############################");
+            }
+            this.Saldo -= valor;
+        }
+        public void Depositar(double valor)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentException("#############################\n Valor do depósito deve ser maior que zero.\n#############################");
+            }
+            this.Saldo += valor;
+        }
+        public bool Transferir(Conta pConta, double pValor) 
+        {
+            try
+            {
+                this.Sacar(pValor);
+                pConta.Depositar(pValor);
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+        
+
+        public abstract string exibirDadosConta();
+    }
+}
